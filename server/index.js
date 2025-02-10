ff1e4659f2770d1deb32d10e7e293f2349ef561e
Pritam Saha <82364650+pritamsaha07@users.noreply.github.com>
2025-02-11 00:57:08 +0530

    first commit

--- server/index.js
+++ server/index.js
@@ -22,7 +22,7 @@ app.use(express.json());
 
 mongoose.connect(
   process.env.MONGODB_URI ||
-    "mongodb+srv://sahapritam2002:<QjudEsr14mkM4qOk>@cluster0.agb2k.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+    "mongodb+srv://sahapritam2002:QjudEsr14mkM4qOk@cluster0.agb2k.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 );
 
 const db = mongoose.connection;
